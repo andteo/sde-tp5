ad7f961788fb93544ca4918325d5eadcfe469002
Andrei Teodoru <andreiteodoru@aol.com>
2022-03-29 09:21:38 +0300

    ex2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use nix::libc::fork;
+use std::{thread, time};
+use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write, Pid, getpid, getppid}};
 
 fn main() {
     /*let mut line =String::new();
@@ -8,7 +9,19 @@ fn main() {
     b.push_str(lstr);
     println!("{}",b);*/ //ex1
 
-    unsafe{
-        fork()
+    match unsafe{fork()}{
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!("Eu sunt parintele, pidul meu e {}, iar copilul are pid {}",getpid(), child);
+            println!("Astept sa termine copilul");
+            thread::sleep(time::Duration::from_secs(1));
+            waitpid(child, None).unwrap();
+            println!("A terminat, gata");
+        }
+        Ok(ForkResult::Child) => {
+            println!("Eu sunt copilul, am pidul {}, iar parintele are pid {}", getpid(), getppid());
+            thread::sleep(time::Duration::from_secs(5));
+            println!("Am asteptat 5 secunde inainte sa afisez asta");
+        }
+        Err(_) => println!("Fork failed"),
     }
 }
