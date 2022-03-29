bd034e7674807289793a6f39efbce42c4b0552dd
Andrei Teodoru <andreiteodoru@aol.com>
2022-03-29 09:57:41 +0300

    final de lab

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::ffi::{CStr, CString};
+use std::fs::File;
 use std::{thread, time};
-use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write, Pid, getpid, getppid}};
+use std::process::Command;
+use nix::libc::dup2;
+use nix::unistd::execvp;
+use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, Pid, getpid, getppid}};
+use std::os::unix::io::AsRawFd;
 
 fn main() {
     /*let mut line =String::new();
@@ -14,13 +20,19 @@ fn main() {
             println!("Eu sunt parintele, pidul meu e {}, iar copilul are pid {}",getpid(), child);
             println!("Astept sa termine copilul");
             thread::sleep(time::Duration::from_secs(1));
-            waitpid(child, None).unwrap();
-            println!("A terminat, gata");
+            let status = waitpid(child, None).unwrap();
+            println!("A terminat, gata, {:?}",status);
         }
         Ok(ForkResult::Child) => {
             println!("Eu sunt copilul, am pidul {}, iar parintele are pid {}", getpid(), getppid());
             thread::sleep(time::Duration::from_secs(5));
             println!("Am asteptat 5 secunde inainte sa afisez asta");
+            let comanda=CString::new("ls").unwrap();
+            let l= CString::new("-l").unwrap();
+            let mut file = File::create("foo.txt")?;
+            dup2(file.as_raw_fd(), 1);
+            execvp(&comanda, &[comanda.clone(), l]);
+            std::process::exit(30)
         }
         Err(_) => println!("Fork failed"),
     }
