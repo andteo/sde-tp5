9681241af6c5432cb85ca4f4c3737d2005c629d2
Andrei Teodoru <andreiteodoru@aol.com>
2022-03-29 08:59:35 +0300

    e1.1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use nix::libc::fork;
+
 fn main() {
     /*let mut line =String::new();
     std::io::stdin().read_line(&mut line).unwrap();
-    let b=Box::new(String::from(line));
+    let lstr=line.as_str();
+    let mut b=Box::new(String::from("Asta e un text fix".to_string()));
+    b.push_str(lstr);
     println!("{}",b);*/ //ex1
+
+    unsafe{
+        fork()
+    }
 }
